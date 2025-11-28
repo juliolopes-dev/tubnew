0af740b5fda5c906c041782e8a5eaa78a212f6dd
Julio Lopes <juliofranlopes18@gmail.com>
2025-11-28 12:02:53 -0300

    Fix Shorts URL support in frontend

--- public/script.js
+++ public/script.js
@@ -39,7 +39,7 @@ elements.videoPlaceholder.addEventListener('click', () => {
 });
 
 function extractVideoId(url) {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|shorts\/)([^#&?]*).*/;
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
 }
